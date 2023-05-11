4bea967dd2c05a223649512e688f4ba6ad98f8ff
DariusIuga <98218119+DariusIuga@users.noreply.github.com>
2023-05-12 01:14:50 +0300

    Implement sort_procs function

--- Tema2/src/task-2/C_sort_procs.c
+++ Tema2/src/task-2/C_sort_procs.c
@@ -10,66 +10,63 @@ typedef struct
 
 void sort_procs(proc *procs, int len)
 {
-    // Field-ul prio reprezinta prioritatea pe care o are un proces atunci cand acesta ruleaza pe procesor.
-    // Fiecare proces are o astfel de prioritate, de la 1 la 5, 1 reprezentand prioritate maxima de rulare, iar 5 reprezentand prioritate minima de rulare.
-    int i = 0;
-    int swapped = 1;
+    int i = 0, j;
     proc temp;
-
-    goto start;
-
-swap:
-    temp = proc_1;
-    proc_1 = proc_2;
-    proc_2 = temp;
-    swapped = 1;
-
-start:
-    if (swapped)
+loop1:
+    if (i >= len)
     {
-        swapped = 0;
-        i = 0;
-        goto loop;
+        return;
     }
 
-    return;
-
-loop:
-    if (i < len - 1)
+    j = i + 1;
+loop2:
+    if (j >= len)
     {
-        // Pentru a sorta procesele, stabilim urmatoarele reguli :
-        //     1) Procesele trebuie sa apara in ordine crescatoare in functie de prioritate.
-        //     2) Pentru procesele cu aceeasi prioritate, acestea se vor ordona crescator in functie de cuanta de timp.
-        //     3) Pentru procese cu aceeasi prioritate si cu aceeasi cuanta de timp, acestea vor fi ordonate crescator dupa id.
-
-        if ((procs[i].prio < procs[i + 1].prio) || (procs[i].prio == procs[i + 1].prio && procs[i].time > procs[i + 1].time) ||
-            (procs[i].prio == procs[i + 1].prio && procs[i].time == procs[i + 1].time && procs[i].pid > procs[i + 1].pid))
-            {
-                goto swap;
-            }
         i++;
-        goto loop;
+        goto loop1;
     }
-    else
+
+    if (procs[i].prio > procs[j].prio)
     {
-        goto start;
+        goto swap;
     }
+    else if (procs[i].prio == procs[j].prio && procs[i].time > procs[j].time)
+    {
+        goto swap;
+    }
+    else if (procs[i].prio == procs[j].prio && procs[i].time == procs[j].time && procs[i].pid > procs[j].pid)
+    {
+        goto swap;
+    }
+
+next:
+    j++;
+    goto loop2;
+    return;
+swap:
+    temp = procs[i];
+    procs[i] = procs[j];
+    procs[j] = temp;
+    goto next;
 }
 
 int main(void)
 {
     // Exemplu de utilizare
     proc procs[] = {
-        {1, 'B', 50},
-        {2, 'A', 20},
-        {3, 'A', 30},
-        {4, 'C', 30},
-        {5, 'B', 30},
-        {6, 'A', 30},
-        {7, 'A', 20},
-        {8, 'C', 20},
-        {9, 'B', 40},
-        {10, 'C', 40}};
+        {28, '3', 16},
+        {54, '3', 16},
+        {34, '2', 49},
+        {53, '1', 51},
+        {63, '5', 50},
+        {21, '5', 60},
+        {58, '2', 39},
+        {37, '1', 41},
+        {64, '1', 14},
+        {36, '5', 56},
+        {51, '3', 11},
+        {40, '5', 10},
+        {99, '2', 57}};
     int len = sizeof(procs) / sizeof(proc);
 
     sort_procs(procs, len);
@@ -77,7 +74,7 @@ int main(void)
     int i;
     for (i = 0; i < len; i++)
     {
-        printf("PID: %d, Prio: %c, Time: %d\n", procs[i].pid, procs[i].prio, procs[i].time);
+        printf("%d %c %d\n", procs[i].pid, procs[i].prio, procs[i].time);
     }
 
     return 0;
--- Tema2/src/task-2/sort_procs.asm
+++ Tema2/src/task-2/sort_procs.asm
@@ -20,9 +20,76 @@ sort_procs:
 
     ;; Your code starts here
 
+    ;; Setting all used registers to 0
+    xor ebx, ebx    ;;i
+    xor ecx, ecx    ;;j
+    xor esi, esi
+    xor edi, edi
+
+loop1:
+    cmp ebx, eax
+    jge end
+
+    mov ecx, ebx
+    inc ecx     ;j = i+1
+    mov esi,edi
+    add esi,5
+loop2:
+    cmp ecx,eax
+    jl pass1
+    inc ebx
+    add edi,5
+    jmp loop1
+
+pass1:
+    ; The first if, for sorting the processes by priority
+    push eax
+    xor eax,eax
+    mov ah, byte [edx+edi + proc.prio]
+    mov al, byte [edx+esi + proc.prio]
+    cmp ah,al
+    pop eax
+    jg swap
+
+    ; if (procs[i].prio == procs[j].prio) we check time
+    jne next
+
+    ; The second if, for sorting the processes by time
+    push ebx
+    push ecx
+    xor ebx,ebx
+    xor ecx,ecx
+    mov bx, word [edx+edi + proc.time]
+    mov cx, word [edx+esi + proc.time]
+    cmp bx,cx
+    pop ecx
+    pop ebx
+    jg swap
+
+    ; if (procs[i].time == procs[j].time) we check pid
+    jne next
+
+    ; The third if, for sorting processes by pid
+    push ebx
+    push ecx
+    xor ebx,ebx
+    xor ecx,ecx
+    mov bx, word [edx+edi + proc.pid]
+    mov cx, word [edx+esi + proc.pid]
+    cmp bx,cx
+    pop ecx
+    pop ebx
+    jg swap
 
 
 
+
+next:
+    inc ecx
+    add esi,5
+    jmp loop2
+
+end:
     ;; Your code ends here
     
     ;; DO NOT MODIFY
@@ -30,3 +97,45 @@ sort_procs:
     leave
     ret
     ;; DO NOT MODIFY
+
+swap:
+    ; Exchange the pid of the two processes
+    ; Use edi and esi for storing the fields
+    push eax
+    push ebx
+
+    ; pid
+    xor eax,eax
+    xor ebx,ebx
+
+    mov ax, word [edx+edi + proc.pid]
+    mov bx, word [edx+esi + proc.pid]
+    mov word [edx+edi + proc.pid], bx
+    mov word [edx+esi + proc.pid], ax
+
+    ; prio
+    xor eax,eax
+    xor ebx,ebx
+
+    mov ah, byte [edx+edi + proc.prio]
+    mov al, byte [edx+esi + proc.prio]
+    mov byte [edx+edi + proc.prio], al
+    mov byte [edx+esi + proc.prio], ah
+
+    ;time
+    xor eax,eax
+    xor ebx,ebx
+
+    mov ax, word [edx+edi + proc.time]
+    mov bx, word [edx+esi + proc.time]
+    mov word [edx+edi + proc.time], bx
+    mov word [edx+esi + proc.time], ax
+
+    pop ebx
+    pop eax
+
+
+    jmp next
+
+
+
