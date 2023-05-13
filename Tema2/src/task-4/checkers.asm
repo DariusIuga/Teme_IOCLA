9436c67380bf654d728029696aa633981a007f34
DariusIuga <98218119+DariusIuga@users.noreply.github.com>
2023-05-13 14:32:01 +0300

    Add files via upload

--- Tema2/src/task-4/checkers.asm
+++ Tema2/src/task-4/checkers.asm
@@ -17,42 +17,43 @@ checkers:
     ;; DO NOT MODIFY
     ;; FREESTYLE STARTS HERE
 
-    ; Offset of the current element from the start of the table
+    ; The matrix is indexed unconventionally.
+    ; edx stored the offset of the current element from the start of the table
     ; (bottom-left)
     lea edx, [eax*8 + ebx]
 
     ; top-left
+    cmp eax,6
+    jg pass_top_left
     cmp ebx,1
     jl pass_top_left
-    cmp eax,1
-    jl pass_top_left
-    mov [ecx + edx-9], dword 1
+    mov [ecx + edx+7], dword 1
 
 pass_top_left:
     ; top-right
-    cmp ebx,1
-    jl pass_top_right
     cmp eax,6
     jg pass_top_right
-    mov [ecx + edx-7], dword 1
+    cmp ebx,6
+    jg pass_top_right
+    mov [ecx + edx+9], dword 1
 
 
 pass_top_right:
     ; bottom-left
-    cmp ebx,6
-    jg pass_bottom_left
     cmp eax,1
     jl pass_bottom_left
-    mov [ecx + edx+7], dword 1
+    cmp ebx,1
+    jl pass_bottom_left
+    mov [ecx + edx-9], dword 1
 
 
 pass_bottom_left:
     ; bottom-right
+    cmp eax,1
+    jl pass_bottom_right
     cmp ebx,6
     jg pass_bottom_right
-    cmp eax,6
-    jg pass_bottom_right
-    mov [ecx + edx+9], dword 1
+    mov [ecx + edx-7], dword 1
 
 
 pass_bottom_right:
